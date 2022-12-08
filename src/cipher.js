c2ebc269073e3b509a27504d81b35608ce94e0c2
vanessa <vanne.sna@gmail.com>
2022-12-08 11:55:43 -0600

    proyecto terminado sin mejoras

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,141 @@
 const cipher = {
-  // ...
-};
+
+  //encode
+  encode: function(offset, mensaje) //Función con argumentos mensaje y desplazamiento
+  {
+    if (offset <= 0 && mensaje !== 'string'){
+      throw new TypeError();
+    }
+    let resultado = "";
+     
+    for (let i = 0; i < mensaje.length; i++)
+    {
+      const character = mensaje.charCodeAt(i);
+      let encode;
+
+      if(character >= 65 && character <= 90){
+       
+        const numberEncode = ((character - 65 + parseInt(offset)) %26) + 65; 
+        encode = String.fromCharCode(numberEncode);
+
+      }
+      if(character === 32)
+      {
+        encode = String.fromCharCode(character);
+      }
+      
+      resultado = resultado + encode;
+    }
+  
+      
+    return resultado;  
+  },     
+
+  //decode
+  decode: function (offset, mensaje)
+  {
+    if(offset <= 0 && mensaje !== "string"){
+      throw new TypeError();
+    }
+    
+
+    let resultado = "";
+     
+    for (let i = 0; i < mensaje.length; i++)
+    {
+      let decode;
+      const character = mensaje.charCodeAt(i);
+
+      if(character >= 65 && character <= 90){
+        const numberEncode = ((character - 90 - parseInt(offset)) %26) +90;
+        decode = String.fromCharCode(numberEncode);
+      }
+      
+      if(character === 32){
+        decode = String.fromCharCode(character);
+      }
+
+      resultado = resultado + decode;
+    }
+
+   
+   
+    return resultado;  
+
+  } 
+
+}; 
+
 
 export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+       ----------------
+       
+       encode: function(offsetCifrar, originalText){
+     if (offsetCifrar <= 0 && typeof originalText != "string") {
+       throw new TypeError();
+     }
+
+     let textEncode = '';
+               
+    
+     for (let i = 0; i < originalText.length; i++) {
+      
+       const numberByCharacter = originalText.charCodeAt(i);
+       
+       if (numberByCharacter >= 65 && numberByCharacter <=90) {
+         const textWithNumberEncode = ((numberByCharacter - 65 + parseInt(offsetCifrar)) % 26 ) + 65;
+         const textWithLetterEncode = String.fromCharCode(textWithNumberEncode);
+         textEncode += textWithLetterEncode;
+       } else if (numberByCharacter >= 97 && numberByCharacter <=122) {
+         const textWithNumberEncode = ((numberByCharacter - 97 + parseInt(offsetCifrar)) % 26) + 97;
+         const textWithLetterEncode = String.fromCharCode(textWithNumberEncode);
+         textEncode += textWithLetterEncode;
+
+       } else {
+         textEncode += String.fromCharCode(numberByCharacter);
+         }  
+         
+     }
+         
+        
+
+   return textEncode;
+          
+
+   }, */
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+//window.addEventListener("load", inicio, true); //Terminó de cargar los recursos
+
+//function inicio()
+//{
+//Botón para codificar
+document.getElementById("encodeButton").addEventListener("click", function()
+{
+  const mensaje = document.getElementById("Messaje").value.toUpperCase(); //Convierte el mensaje a mayúsculas 
+  const offset = document.getElementById("offset").value; //Valor de desplazamiento
+      
+  document.getElementById("mensajeCifrado").value = cipher.encode(offset, mensaje); //Valor del mensaje codificado
+});
+
+//Botón para decodificar
+document.getElementById("decodeButton").addEventListener("click", function()
+{
+  const mensaje = document.getElementById("Messaje").value.toUpperCase(); //Convierte el mensaje a mayúsculas
+  const offset = document.getElementById("offset").value; //Valor de desplazamiento
+     
+  document.getElementById("mensajeCifrado").value = cipher.decode(offset, mensaje); //Valor del mensaje decodificado
+});
+//}
+  
